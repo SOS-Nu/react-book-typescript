0a1676fb90c3e844f05dea8fe4fd081180710f98
nhokanhanh@gmail.com <sosnu1412@gmail.com>
2025-05-05 16:09:04 +0700

    filter sort vs homepage;

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,119 +1,169 @@
-import axios from 'services/axios.customize';
+import axios from "services/axios.customize";
 
 export const loginAPI = (username: string, password: string) => {
-    const urlBackend = "/api/v1/auth/login";
-    return axios.post<IBackendRes<ILogin>>(urlBackend, { username, password }, {
-        headers: {
-            delay: 3000
-        }
-    })
-}
+  const urlBackend = "/api/v1/auth/login";
+  return axios.post<IBackendRes<ILogin>>(
+    urlBackend,
+    { username, password },
+    {
+      headers: {
+        delay: 1500,
+      },
+    }
+  );
+};
 
-export const registerAPI = (fullName: string, email: string, password: string, phone: string) => {
-    const urlBackend = "/api/v1/user/register";
-    return axios.post<IBackendRes<IRegister>>(urlBackend, { fullName, email, password, phone })
-}
+export const registerAPI = (
+  fullName: string,
+  email: string,
+  password: string,
+  phone: string
+) => {
+  const urlBackend = "/api/v1/user/register";
+  return axios.post<IBackendRes<IRegister>>(urlBackend, {
+    fullName,
+    email,
+    password,
+    phone,
+  });
+};
 
 export const fetchAccountAPI = () => {
-    const urlBackend = "/api/v1/auth/account";
-    return axios.get<IBackendRes<IFetchAccount>>(urlBackend, {
-        headers: {
-            delay: 1000
-        }
-    })
-}
+  const urlBackend = "/api/v1/auth/account";
+  return axios.get<IBackendRes<IFetchAccount>>(urlBackend, {
+    headers: {
+      delay: 1500,
+    },
+  });
+};
 
 export const logoutAPI = () => {
-    const urlBackend = "/api/v1/auth/logout";
-    return axios.post<IBackendRes<IRegister>>(urlBackend)
-}
+  const urlBackend = "/api/v1/auth/logout";
+  return axios.post<IBackendRes<IRegister>>(urlBackend);
+};
 
 export const getUsersAPI = (query: string) => {
-    const urlBackend = `/api/v1/user?${query}`;
-    return axios.get<IBackendRes<IModelPaginate<IUserTable>>>(urlBackend)
-}
+  const urlBackend = `/api/v1/user?${query}`;
+  return axios.get<IBackendRes<IModelPaginate<IUserTable>>>(urlBackend);
+};
 
-export const createUserAPI = (fullName: string, email: string,
-    password: string, phone: string) => {
-    const urlBackend = "/api/v1/user";
-    return axios.post<IBackendRes<IRegister>>(urlBackend,
-        { fullName, email, password, phone })
-}
+export const createUserAPI = (
+  fullName: string,
+  email: string,
+  password: string,
+  phone: string
+) => {
+  const urlBackend = "/api/v1/user";
+  return axios.post<IBackendRes<IRegister>>(urlBackend, {
+    fullName,
+    email,
+    password,
+    phone,
+  });
+};
 
-export const bulkCreateUserAPI = (hoidanit: {
+export const bulkCreateUserAPI = (
+  hoidanit: {
     fullName: string;
     password: string;
     email: string;
     phone: string;
-}[]) => {
-    const urlBackend = "/api/v1/user/bulk-create";
-    return axios.post<IBackendRes<IResponseImport>>(urlBackend, hoidanit)
-}
+  }[]
+) => {
+  const urlBackend = "/api/v1/user/bulk-create";
+  return axios.post<IBackendRes<IResponseImport>>(urlBackend, hoidanit);
+};
 
 export const updateUserAPI = (_id: string, fullName: string, phone: string) => {
-    const urlBackend = "/api/v1/user";
-    return axios.put<IBackendRes<IRegister>>(urlBackend,
-        { _id, fullName, phone })
-}
-
+  const urlBackend = "/api/v1/user";
+  return axios.put<IBackendRes<IRegister>>(urlBackend, {
+    _id,
+    fullName,
+    phone,
+  });
+};
 
 export const deleteUserAPI = (_id: string) => {
-    const urlBackend = `/api/v1/user/${_id}`;
-    return axios.delete<IBackendRes<IRegister>>(urlBackend)
-}
-
+  const urlBackend = `/api/v1/user/${_id}`;
+  return axios.delete<IBackendRes<IRegister>>(urlBackend);
+};
 
 export const getBooksAPI = (query: string) => {
-    const urlBackend = `/api/v1/book?${query}`;
-    return axios.get<IBackendRes<IModelPaginate<IBookTable>>>(urlBackend)
-}
+  const urlBackend = `/api/v1/book?${query}`;
+  return axios.get<IBackendRes<IModelPaginate<IBookTable>>>(urlBackend, {
+    headers: {
+      delay: 1500,
+    },
+  });
+};
 
 export const getCategoryAPI = () => {
-    const urlBackend = `/api/v1/database/category`;
-    return axios.get<IBackendRes<string[]>>(urlBackend);
-}
+  const urlBackend = `/api/v1/database/category`;
+  return axios.get<IBackendRes<string[]>>(urlBackend);
+};
 
 export const uploadFileAPI = (fileImg: any, folder: string) => {
-    const bodyFormData = new FormData();
-    bodyFormData.append('fileImg', fileImg);
-    return axios<IBackendRes<{
-        fileUploaded: string
-    }>>({
-        method: 'post',
-        url: '/api/v1/file/upload',
-        data: bodyFormData,
-        headers: {
-            "Content-Type": "multipart/form-data",
-            "upload-type": folder
-        },
-    });
-}
-
+  const bodyFormData = new FormData();
+  bodyFormData.append("fileImg", fileImg);
+  return axios<
+    IBackendRes<{
+      fileUploaded: string;
+    }>
+  >({
+    method: "post",
+    url: "/api/v1/file/upload",
+    data: bodyFormData,
+    headers: {
+      "Content-Type": "multipart/form-data",
+      "upload-type": folder,
+    },
+  });
+};
 
 export const createBookAPI = (
-    mainText: string, author: string,
-    price: number, quantity: number, category: string,
-    thumbnail: string, slider: string[]
+  mainText: string,
+  author: string,
+  price: number,
+  quantity: number,
+  category: string,
+  thumbnail: string,
+  slider: string[]
 ) => {
-    const urlBackend = "/api/v1/book";
-    return axios.post<IBackendRes<IRegister>>(urlBackend,
-        { mainText, author, price, quantity, category, thumbnail, slider })
-}
-
+  const urlBackend = "/api/v1/book";
+  return axios.post<IBackendRes<IRegister>>(urlBackend, {
+    mainText,
+    author,
+    price,
+    quantity,
+    category,
+    thumbnail,
+    slider,
+  });
+};
 
 export const updateBookAPI = (
-    _id: string,
-    mainText: string, author: string,
-    price: number, quantity: number, category: string,
-    thumbnail: string, slider: string[]
+  _id: string,
+  mainText: string,
+  author: string,
+  price: number,
+  quantity: number,
+  category: string,
+  thumbnail: string,
+  slider: string[]
 ) => {
-    const urlBackend = `/api/v1/book/${_id}`;
-    return axios.put<IBackendRes<IRegister>>(urlBackend,
-        { mainText, author, price, quantity, category, thumbnail, slider })
-}
-
+  const urlBackend = `/api/v1/book/${_id}`;
+  return axios.put<IBackendRes<IRegister>>(urlBackend, {
+    mainText,
+    author,
+    price,
+    quantity,
+    category,
+    thumbnail,
+    slider,
+  });
+};
 
 export const deleteBookAPI = (_id: string) => {
-    const urlBackend = `/api/v1/book/${_id}`;
-    return axios.delete<IBackendRes<IRegister>>(urlBackend)
-}
+  const urlBackend = `/api/v1/book/${_id}`;
+  return axios.delete<IBackendRes<IRegister>>(urlBackend);
+};
